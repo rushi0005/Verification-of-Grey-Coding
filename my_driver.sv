0cabfbc221ddff3ecf62a45579a68b570a7cf39b
rushi0005 <64946315+rushi0005@users.noreply.github.com>
2021-02-27 14:01:38 -0800

    Update my_driver.sv

--- my_driver.sv
+++ my_driver.sv
@@ -1,7 +1,7 @@
 class my_driver extends uvm_driver #(my_seq_item) ;
   `uvm_component_utils(my_driver)
   my_seq_item res ;
-  
+  virtual gi xx   ;
   function new (string name = "my_driver", uvm_component parent );
     super.new(name,parent);
   endfunction
@@ -11,16 +11,53 @@ class my_driver extends uvm_driver #(my_seq_item) ;
     //res = my_seq_item :: type_id :: create("res",this);
   endfunction
   
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    if (uvm_config_db#(virtual gi)::get(null,"dp","intf",xx));
+    else begin
+      `uvm_fatal("config", "DIdnot get intf");
+    end
+    endfunction
+  
   virtual task run_phase(uvm_phase phase);
     super.run_phase(phase);
     forever begin 
      res = new() ;
      seq_item_port.get_next_item(res);
-     drive(res);
+      case(res.cmd)
+        Dreset : do_reset(res);
+        Dwrite : do_write(res);
+        Dread : do_read(res);
+      endcase
+     //drive(res);
      seq_item_port.item_done();
    end
   endtask
-  task drive (my_seq_item res );
-    $display("Value of data %0h", res.data);
+  task do_reset (my_seq_item m );
+    xx.reset = 1 ;
+    xx.addr = 0 ;
+    xx.datain = 0 ;
+    xx.rw = 0 ;
+    @(posedge (xx.clk)) #1
+    xx.reset = 0 ;
+  endtask
+  
+  task do_write (my_seq_item m);
+    xx.addr = m.addr ;
+    xx.datain = m.data ;
+    xx.rw = 1 ;
+    @(posedge (xx.clk))
+    xx.rw = 0 ;
+    xx.addr = 32'h0bad ;
+  endtask
+  
+  
+  task do_read (my_seq_item m);
+    xx.addr = m.addr ;
+    xx.datain = m.data ;
+    xx.rw = 0 ;
+    @(posedge (xx.clk))
+    xx.rw = 0 ;
+    xx.addr = 32'h0bad ;
   endtask
 endclass
