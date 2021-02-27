4b0e113e22d3317258729169d9b50da04d0dc44e
rushi0005 <64946315+rushi0005@users.noreply.github.com>
2021-02-27 13:56:52 -0800

    Update Testbench.sv

--- Testbench.sv
+++ Testbench.sv
@@ -2,7 +2,7 @@
 // or browse Examples
 // Code your testbench here
 // or browse Examples
-// sequence sequencer Driver Testbench top module 
+// sequence sequencer Driver monitor scoreboard Testbench top module 
 `include "interface.sv"
 package ydv ;
  import uvm_pkg :: * ;
@@ -10,10 +10,12 @@ package ydv ;
 		
 
 	`include "my_seq_item.sv"
+	`include "mimsg.sv"
 	`include "my_seq.sv"
-	
+	`include "my_scoreboard.sv"
 	`include "my_sequencer.sv"
     `include "my_driver.sv"
+	`include "my_monitor.sv"
 	`include "my_agent.sv"
     `include "my_test.sv" 
 
@@ -25,10 +27,22 @@ endpackage
 //import uvm_pkg :: * ;
 `timescale 1ns/10ps
 module top ();
-  reg clk, reset ;
+  reg clk ;
   import uvm_pkg :: * ;
-  gi intf (clk,reset);
+  
+  initial begin
+    clk = 0 ;
+    
+    forever begin 
+      #5 clk = ~clk ;
+    end
+  end
+  
+  
+  gi intf (clk);
   grey_scale go (intf.DUT);
+  
+  
   initial begin 
     $display("I am inside top module");
   end
@@ -36,10 +50,16 @@ module top ();
   end
   
   initial begin
+  
+    uvm_config_db#(virtual gi)::set(null,"dp","intf", intf);
     run_test("my_test");
   end
-  
+  initial begin
+    $dumpfile("my_file.vcd");
+    $dumpvars(0,top);
+  end
   
 endmodule
 
 `include "design1.sv"
+ 
